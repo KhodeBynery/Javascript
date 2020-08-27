169d35961513a1c11c9a30eb3671fc5f6a32ceb6
Pedro Martinez <martinezpt3@gmail.com>
2020-08-27 19:09:57 -0400

    Test moving console message

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict'
-console.log('Here\'s a hidden message');
+
 
 // This is a single-line comment.
 
@@ -18,3 +18,5 @@ let today = new Date();
 let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+console.log('Here\'s a hidden message');
